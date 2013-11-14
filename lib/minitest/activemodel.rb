d62317f596775cac1f0d7f749f896526d2279baf
Jerry Cheung <jch@whatcodecraves.com>
2013-11-13 19:08:12 -0800

    generated gem

---
+++ lib/minitest/activemodel.rb
@@ -0,0 +1,7 @@
+require "minitest/activemodel/version"
+
+module Minitest
+  module Activemodel
+    # Your code goes here...
+  end
+end
---
+++ lib/minitest/activemodel/version.rb
@@ -0,0 +1,5 @@
+module Minitest
+  module Activemodel
+    VERSION = "0.0.1"
+  end
+end
