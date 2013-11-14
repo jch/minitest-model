740a91436a762ae5dda3e221683c43e0c65f93f8
Jerry Cheung <jch@whatcodecraves.com>
2013-11-13 19:44:51 -0800

    add code

--- lib/minitest/activemodel.rb
+++ lib/minitest/activemodel.rb
@@ -1,7 +1,33 @@
 require "minitest/activemodel/version"
 
-    # Your code goes here...
 module MiniTest
   module ActiveModel
+    ########### ActiveModel::Validations
+
+    # Public: assert `record` is valid. If optional argument `attribute` is
+    # given, only assert that no errors are on that attribute.
+    #
+    # See test/test.rb for sample usage
+    #
+    def assert_valid(record, attribute = nil)
+      if attribute
+        record.valid?
+        errors = record.errors[attribute]
+        assert errors.blank?, "Validation errors on #{attribute}:\n" + errors.join("\n")
+      else
+        assert record.valid?, "Validation errors:\n" + record.errors.full_messages.join("\n")
+      end
+    end
+
+    # Public: the inverse of assert_valid
+    def refute_valid(record, attribute = nil)
+      valid = record.valid?
+      if attribute
+        errors = record.errors[attribute]
+        refute errors.empty?, "Expected validation error on #{attribute}"
+      else
+        refute valid, "Expected validation errors"
+      end
+    end
   end
 end
---
+++ test/test.rb
@@ -0,0 +1,34 @@
+require 'minitest/activemodel'
+require 'minitest/autorun'
+
+require 'ostruct'
+require 'active_model'
+class Person < OpenStruct
+  include ActiveModel::Validations
+
+  validates_presence_of :name, :email
+end
+
+class PersonTest < MiniTest::Unit::TestCase
+  include MiniTest::ActiveModel
+
+  def setup
+    @person = Person.new
+  end
+
+  def test_invalid
+    refute_valid @person
+  end
+
+  def test_valid
+    @person.name  = "Jerry"
+    @person.email = "jerry@example.com"
+    assert_valid @person
+  end
+
+  def test_requires_name
+    refute_valid @person, :name
+    @person.name = "Jerry"
+    assert_valid @person, :name
+  end
+end
